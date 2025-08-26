0751f70e4082bfb712e53becaf69819fc5358275
X_ChenD_Hai <illuminatestar@foxmail.com>
2025-08-26 18:10:05 +0800

    feat(core): 添加项目基础配置文件和主要源代码
    项目基础配置文件和主要源代码的添加，包括`.clang-format`、`.gitignore`、`CMakeLists.txt`、`src/CMakeLists.txt`、`src/main.cc`和`src/public.h`。
    
    - 添加`.clang-format`文件以指定代码格式化规则，设置为Google风格，C++语言，缩进宽度为4。
    - 添加`.gitignore`文件以忽略不必要的文件和目录，同时保留`src`目录、`.clang-format`文件、`CMakeLists.txt`和`.gitignore`文件。
    - 添加`CMakeLists.txt`文件以配置项目的基本信息，包括CMake最低版本要求、项目名称和版本号、所需的库（GTest和xclogger）、C++标准版本以及其他编译选项。
    - 添加`src/CMakeLists.txt`文件以指定源代码目录下的构建规则，创建一个可执行文件`xcal`，并将其源文件`main.cc`添加到项目中，同时链接所需的库`xclogger::zmq_support`。
    - 添加`src/main.cc`文件作为程序的入口点，包含必要的头文件并启用异步ZMQ日志提交流，程序启动时记录日志信息。
    - 添加`src/public.h`文件以提供公共宏定义和头文件包含，支持文件名、行号和函数名的日志跟踪，包含必要的xclogger库头文件。

---
+++ src/main.cc
@@ -0,0 +1,8 @@
+#include "public.h"
+
+XCLOG_ENABLE_ASYNCZMQLOGSUBMITSTREAM("tcp://127.0.0.1:5555")
+
+int main(int argc, char **argv) {
+  XLOG(0, envman, main) << "START APP";
+  return 0;
+}
---
+++ src/public.h
@@ -0,0 +1,5 @@
+#define XCLOG_FILE_TRACK
+#define XCLOG_LINE_TRACK
+#define XCLOG_FUNCTION_TRACK
+#include <xclogger/asynczmqlogsubmitstream.hpp>
+#include <xclogger/logger.hpp>
