6acdd7f78f5daa4aa380956fed34911a8ac95bc8
X_ChenD_Hai <illuminatestar@foxmail.com>
2025-09-01 23:43:02 +0800

    refactor(opengl): 将 ShaderInstance 从 struct 改为 class 并调整成员变量访问修饰符
    将 ShaderInstance 从 struct 改为 class，提高代码的封装性。同时，将成员变量 shader 改为私有成员变量 shader_，并更新相应的调用代码以保持功能一致。

--- xcal/render/impl/opengl/utils/shaderinstence.hpp
+++ xcal/render/impl/opengl/utils/shaderinstence.hpp
@@ -6,20 +6,23 @@ std::shared_ptr<xcal::render::opengl::GL::ShaderProgram> create_shader() {
     static_assert(false, "Not implemented");
 };
 template <class T, size_t _id>
-struct ShaderInstance {
-    static std::weak_ptr<xcal::render::opengl::GL::ShaderProgram> shader;
+class ShaderInstance {
+   private:
+    static std::weak_ptr<xcal::render::opengl::GL::ShaderProgram> shader_;
+
+   public:
     static std::shared_ptr<xcal::render::opengl::GL::ShaderProgram> instance() {
-        if (shader.expired()) {
+        if (shader_.expired()) {
             auto tmp = create_shader<T, _id>();
-            shader = tmp;
+            shader_ = tmp;
             return tmp;
         }
-        return shader.lock();
+        return shader_.lock();
     }
 };
 template <class T, size_t _id>
 std::weak_ptr<xcal::render::opengl::GL::ShaderProgram>
-    ShaderInstance<T, _id>::shader{};
+    ShaderInstance<T, _id>::shader_{};
 }  // namespace xcal::render::opengl::utils
 
 #define XCAL_SHADER_INSTANCE(T, _id)                         \
