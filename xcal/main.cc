1b4769a2374945fa07461c0d941ed4e10798607f
X_ChenD_Hai <illuminatestar@foxmail.com>
2025-09-02 14:35:58 +0800

    style(xcal): 使用命名空间简化类型引用
    在 `main.cc` 中添加了 `using namespace xcal::mobject;` 声明，使得后续代码中可以直接使用 `Circle` 和 `Line` 而无需前缀 `xcal::mobject::`。这提高了代码的可读性。
    
    在 `mobject_types.hpp` 中对命名空间布局进行了调整，将类声明移到命名空间内部，并在 `to_string` 函数中添加了命名空间前缀 `xcal::mobject::`。这确保了 `to_string` 函数可以正确识别 `xcal::mobject::Type` 枚举类型，同时保持了类声明的清晰结构。

--- xcal/main.cc
+++ xcal/main.cc
@@ -7,10 +7,11 @@
 int main(int argc, char **argv) {
     XCAL_INFO(XCAL, APP) << "app start";
     auto scene = std::make_unique<xcal::scene::Scene>();
+    using namespace xcal::mobject;
 
-    scene->add<xcal::mobject::Circle>()->set_stroke_color({0, 0.5, 0.5});
-    scene->add<xcal::mobject::Line>(2)->set_stroke_color({1, 0, 0});
-    scene->add<xcal::mobject::Line>(2)->set_stroke_color({0, 1, 0})->rotate(90);
+    scene->add<Circle>()->set_stroke_color({0, 0.5, 0.5});
+    scene->add<Line>(2)->set_stroke_color({1, 0, 0});
+    scene->add<Line>(2)->set_stroke_color({0, 1, 0})->rotate(90);
     auto render = xcal::render::opengl::OpenGLRender(scene.get());
     render.show();
     XCAL_INFO(XCAL, APP) << "app end";
