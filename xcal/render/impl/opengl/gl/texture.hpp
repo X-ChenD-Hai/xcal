71e1ffaef866ce5a83c6930145091ce48a50b5d8
X_ChenD_Hai <illuminatestar@foxmail.com>
2025-09-07 11:09:20 +0800

    feat(render): 添加帧缓冲对象和纹理类的实现
    - 添加了 `FramebufferObject` 类，用于管理OpenGL帧缓冲对象，提供了绑定、解绑、读取像素等功能。
    - 实现了 `FramebufferObject::unbind` 方法，用于解除当前绑定的帧缓冲对象。
    - 实现了 `FramebufferObject` 构造函数，初始化帧缓冲对象并绑定纹理。
    - 实现了 `FramebufferObject::bind` 方法，用于绑定帧缓冲对象。
    - 实现了 `FramebufferObject::read_pixels_char` 和 `read_pixels_float` 方法，用于读取帧缓冲对象中的像素数据。
    - 添加了 `Texture` 类，用于管理OpenGL纹理对象，提供了绑定、解绑、设置参数等功能。
    - 实现了 `Texture::bind` 和 `unbind` 方法，用于绑定和解除绑定纹理对象。
    - 实现了 `Texture::parameteri` 方法，用于设置纹理对象的参数。
    - 实现了 `Texture::image_2d` 方法，用于创建二维纹理图像。
    - 添加了 `Texture::to_gl_enum` 方法，用于将颜色格式和像素类型转换为OpenGL枚举类型。

---
+++ xcal/render/impl/opengl/gl/texture.hpp
@@ -0,0 +1,50 @@
+#include <xcal/public.h>
+
+#include <xcal/render/impl/opengl/core/typedef.hpp>
+
+namespace xcal::render::opengl::GL {
+class FramebufferObject;
+class Texture {
+    friend class ::xcal::render::opengl::GL::FramebufferObject;
+
+   public:
+    enum class color_format_t {
+        RGB,
+        RGBA,
+        DEPTH,
+        STENCIL,
+    };
+    enum class pixel_type_t {
+        UNSIGNED_BYTE,
+        FLOAT,
+    };
+
+   private:
+    gl::GLuint texture_ = 0;
+    gl::GLenum target_{};
+
+   public:
+    Texture(const Texture &) = delete;
+    Texture(Texture &&o) { swap(o); }
+    Texture &operator=(const Texture &) = delete;
+    Texture &operator=(Texture &&o) {
+        swap(o);
+        return *this;
+    }
+    Texture(gl::GLenum target);
+    ~Texture();
+    bool_t is_valid() const { return texture_ != 0; }
+    void image_2d(gl::GLint level, color_format_t internalformat,
+                  gl::GLsizei width, gl::GLsizei height, gl::GLint border,
+                  color_format_t format, pixel_type_t type,
+                  const void *data) const;
+    void parameteri(gl::GLenum pname, gl::GLenum param) const;
+    void bind(gl::GLenum target) const;
+    void bind() const;
+    void unbind();
+    void swap(Texture &o);
+    static void unbind(gl::GLenum target);
+    static gl::GLenum to_gl_enum(color_format_t format);
+    static gl::GLenum to_gl_enum(pixel_type_t type);
+};
+}  // namespace xcal::render::opengl::GL
