175b1fe9176a50aac91f172016af00a6185d9b67
X_ChenD_Hai <illuminatestar@foxmail.com>
2025-09-09 17:27:17 +0800

    feat(animation): 添加插值功能并在场景中应用
    新增了 `xcal/animation/Interpolation.hpp` 的包含，引入了插值功能。
    在 `main` 函数中，为第一个圆添加了 `translate` 和 `scale` 操作。
    使用 `ScalerInterpolation` 对第一个圆的 `scale_x()` 属性进行了线性插值的设置。
    添加了 `Timeline` 的实例化，用于管理动画时间线。
    将 `PerspectiveCamera` 的添加方式从 `std::make_unique` 改为直接构造，并添加到场景中。

--- xcal/main.cc
+++ xcal/main.cc
@@ -1,6 +1,7 @@
 #include <xcal/public.h>
 
 #include <memory>
+#include <xcal/animation/Interpolation.hpp>
 #include <xcal/animation/core/timeline.hpp>
 #include <xcal/camera/perspectivecamera.hpp>
 #include <xcal/mobject/mobject_all.hpp>
@@ -16,16 +17,23 @@ int main(int argc, char **argv) {
     using namespace xcal::animation;
 
     // XCAL_INFO(XCAL, APP) << "cc pos: " << cc->pos().value();
-    scene->add<Line>(3)->set_pos({0, 0})->set_stroke_color({1, 0, 0, 5});
-    auto cc = scene->add<Circle>()->set_radius(1.f / 2)->set_stroke_color(
-        {0.5, 0, 0.5});
-    scene->add<Circle>()->set_radius(0.5f / 2)->set_stroke_color({0, 0.5, 0.5});
-    scene->add<Line>(2)->set_stroke_color({0, 0, 1})->rotate(45);
-    scene->add<Line>(2)->set_stroke_color({0, 1, 0})->rotate(90);
-    scene->add(std::make_unique<PerspectiveCamera>(45.0, 16 / 9.0, 0.1, 1000.0))
+    auto l1 =
+        scene->add<Line>(3)->set_pos({0, 0})->set_stroke_color({1, 0, 0, 5});
+    auto c1 = scene->add<Circle>();
+    c1->set_radius(1.f / 2)
+        ->set_stroke_color({0.5, 0, 0.5})
+        ->translate(0, 1)
+        ->scale(0.5);
+    auto c2 = scene->add<Circle>()->set_radius(0.5f / 2)->set_stroke_color(
+        {0, 0.5, 0.5});
+    auto l2 = scene->add<Line>(2)->set_stroke_color({0, 0, 1})->rotate(45);
+    auto l3 = scene->add<Line>(2)->set_stroke_color({0, 1, 0})->rotate(90);
+    scene->add<PerspectiveCamera>(45.0, 16 / 9.0, 0.1, 1000.0)
         ->set_position(0, 0, 3)
         ->set_target(0, 0, 0);
-    // scene->add<Timeline>(1.f);
+    auto ani = scene->add<ScalerInterpolation>(&c1->scale_x(), 0, 1,
+                                               interpolation_functions::linear);
+    scene->add<Timeline>();
 
     auto render = xcal::render::opengl::OpenGLRender{scene.get()};
     render.default_camera()
